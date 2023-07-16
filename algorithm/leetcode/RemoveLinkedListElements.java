e4920f94292ea6fe6fe3e90ad4c94f165f23e97d
RuinedofJoker <616401600@qq.com>
2023-07-16 10:44:34 +0800

    337. 打家劫舍 III

--- algorithm/leetcode/RemoveLinkedListElements.java
+++ algorithm/leetcode/RemoveLinkedListElements.java
@@ -33,19 +33,4 @@ public class RemoveLinkedListElements {
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
 
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
