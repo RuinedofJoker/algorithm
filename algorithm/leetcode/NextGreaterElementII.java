aae7c02d889a841fc748491a9d07723c5345d732
RuinedofJoker <616401600@qq.com>
2023-08-06 21:28:13 +0800

    503. 下一个更大元素 II

---
+++ algorithm/leetcode/NextGreaterElementII.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 503. 下一个更大元素 II
+ *
+ * 给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的 下一个更大元素 。
+ *
+ * 数字 x 的 下一个更大的元素 是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1 。
+ *
+ * 示例 1:
+ * 输入: nums = [1,2,1]
+ * 输出: [2,-1,2]
+ * 解释: 第一个 1 的下一个更大的数是 2；
+ * 数字 2 找不到下一个更大的数；
+ * 第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+ *
+ * 示例 2:
+ * 输入: nums = [1,2,3,4,3]
+ * 输出: [2,3,4,-1,4]
+ *
+ * 提示:
+ * 1 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ */
+public class NextGreaterElementII {
+    public static void main(String[] args) {
+        //new NextGreaterElementII().nextGreaterElements(new int[]{5,4,3,2,1});
+        new NextGreaterElementII().nextGreaterElements(new int[]{1,2,3,2,1});
+    }
+    public int[] nextGreaterElements(int[] nums) {
+        if (nums.length == 1)   return new int[]{-1};
+
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums.length * 2; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % nums.length]) {
+                result[stack.pop()] = nums[i % nums.length];
+            }
+            stack.push(i % nums.length);
+        }
+
+        return result;
+    }
+}
