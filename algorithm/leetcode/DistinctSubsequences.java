16befb0dfdf04c5661d2bc11ca0ca04813777726
RuinedofJoker <616401600@qq.com>
2023-08-11 09:16:16 +0800

    583. 两个字符串的删除操作

---
+++ algorithm/leetcode/DistinctSubsequences.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/**
+ * 115. 不同的子序列
+ *
+ * 给你两个字符串 s 和 t ，统计并返回在 s 的 子序列 中 t 出现的个数。
+ *
+ * 题目数据保证答案符合 32 位带符号整数范围。
+ *
+ * 示例 1：
+ * 输入：s = "rabbbit", t = "rabbit"
+ * 输出：3
+ * 解释：
+ * 如下所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+ * rabbbit
+ * rabbbit
+ * rabbbit
+ *
+ * 示例 2：
+ * 输入：s = "babgbag", t = "bag"
+ * 输出：5
+ *
+ * 解释：
+ * 如下所示, 有 5 种可以从 s 中得到 "bag" 的方案。
+ * babgbag
+ * babgbag
+ * babgbag
+ * babgbag
+ * babgbag
+ *
+ * 提示：
+ * 1 <= s.length, t.length <= 1000
+ * s 和 t 由英文字母组成
+ */
+public class DistinctSubsequences {
+    public static void main(String[] args) {
+        System.out.println(new DistinctSubsequences().numDistinct("rabbbit", "rabbit"));
+    }
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 0; i < s.length(); i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[s.length()][t.length()];
+    }
+}
